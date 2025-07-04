524e09ff63b4d9062bac51c3d0df41434240881e
Aditya_ <98688594+LegendPlayz83@users.noreply.github.com>
2025-07-05 00:52:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 const int buttonPins[4] = {6, 7, 0, 1};
 const int ledPins[4] = {26, 27, 28, 29};
 
+unsigned long pressStart[4] = {0, 0, 0, 0};
+bool isPressed[4] = {false, false, false, false};
+int ledBrightness[4] = {0, 0, 0, 0};
+
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
@@ -9,11 +13,32 @@ void setup() {
 }
 
 void loop() {
+  unsigned long now = millis();
+
   for (int i = 0; i < 4; i++) {
-    if (digitalRead(buttonPins[i]) == LOW) {
-      digitalWrite(ledPins[i], HIGH);
-    } else {
-      digitalWrite(ledPins[i], LOW);
+    bool currentPressed = (digitalRead(buttonPins[i]) == LOW);
+
+    if (currentPressed && !isPressed[i]) {
+      pressStart[i] = now;
+      isPressed[i] = true;
     }
+
+    if (!currentPressed && isPressed[i]) {
+      isPressed[i] = false;
+    }
+
+    if (isPressed[i]) {
+      unsigned long heldTime = now - pressStart[i];
+      ledBrightness[i] = constrain(heldTime / 4, 0, 255);  // slower rise
+    } else {
+      if (ledBrightness[i] > 0) {
+        ledBrightness[i] -= 5;  // fade out speed
+        if (ledBrightness[i] < 0) ledBrightness[i] = 0;
+      }
+    }
+
+    analogWrite(ledPins[i], ledBrightness[i]);
   }
+
+  delay(10);  // smooth update
 }
